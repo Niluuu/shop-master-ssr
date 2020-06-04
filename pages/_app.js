eaacb6769dcaf5424ca89ca1ce61e4d866016e61
saidumarovanilufar <saidumarovanilufar@mail.ru>
2020-06-04 16:15:02 +0500

    index page end, have problem with icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import "../styles/style.css";
 import "../styles/bootstrap.min.css";
+import "../styles/font-awsome/font-awesome.css";
 // import "../styles/font-awesome.min.css";
 // import "@fortawesome/fontawesome-free/js/fontawesome";
 // import "@fortawesome/fontawesome-free/js/solid";
 // import "@fortawesome/fontawesome-free/js/regular";
 // import "@fortawesome/fontawesome-free/js/brands";
-import "@fortawesome/fontawesome-free/css/all.css";
+// import "@fortawesome/fontawesome-free/css/all.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
