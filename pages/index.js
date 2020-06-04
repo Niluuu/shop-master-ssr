54dbd20a183ae84c7b3776bdfc93af05dcfb560e
saidumarovanilufar <saidumarovanilufar@mail.ru>
2020-06-04 16:22:39 +0500

    solve problem with icons

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,17 @@ import History from "../src/components/indexPage/history";
 import MainSection from "../src/components/indexPage/mainSection/index.js";
 import CategorySection from "../src/components/indexPage/category/categorySection";
 import Footer from "../src/components/footer";
+import Head from "next/head";
 
 export default function IndexPage() {
   return (
     <div>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="../src/styles/font-awsome/font-awsome.css"
+        />  
+      </Head>
       <Header />
       <ProductSection />
       <History />
